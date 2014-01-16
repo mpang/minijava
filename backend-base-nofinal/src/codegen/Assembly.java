b8946c23afc943aee1758abae1985db53b10f5b2
cs411 <cs411@07e144b1-f7ef-4d70-afd9-7d896f8a2c5e>
2014-01-16 17:50:50 +0000

    git-svn-id: https://kunghit.ugrad.cs.ubc.ca:8411/repos/2013w2/users/j6j7@342 07e144b1-f7ef-4d70-afd9-7d896f8a2c5e

---
+++ backend-base-nofinal/src/codegen/Assembly.java
@@ -0,0 +1,47 @@
+package codegen;
+
+import java.util.Iterator;
+
+import util.DefaultIndentable;
+import util.IndentingWriter;
+import util.List;
+import util.Utils;
+
+
+/**
+ * Represents the result of applying a code generator to a list of IR fragments.
+ */
+public class Assembly extends DefaultIndentable implements Iterable<AssemFragment> {
+
+	private List<AssemFragment> fragments = List.empty();
+	
+	@Override
+	public void dump(IndentingWriter out) {
+		out.indent();
+//		if (sourceFile!=null)
+//			out.println(".file "+sourceFile);
+		out.println(".text");
+		out.outdent();
+		
+		for (AssemFragment f : fragments) {
+			out.println(f);
+		}
+		
+		out.indent();
+		
+		out.println(".ident	\"minijavac: cs411 course project 2011w2\"");
+		if (!Utils.macOS()) 
+			out.println(".section	.note.GNU-stack,\"\",@progbits");
+		out.outdent();
+	}
+
+	public void add(AssemFragment code) {
+		fragments.add(code);
+	}
+
+	@Override
+	public Iterator<AssemFragment> iterator() {
+		return fragments.iterator();
+	}
+
+}
