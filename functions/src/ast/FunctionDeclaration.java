7861453fc15817e4e9af2bf358b7ce82f620d155
j6j7 <j6j7@07e144b1-f7ef-4d70-afd9-7d896f8a2c5e>
2014-01-23 21:08:52 +0000

    Statement and FunctionDeclaration
    git-svn-id: https://kunghit.ugrad.cs.ubc.ca:8411/repos/2013w2/users/j6j7@533 07e144b1-f7ef-4d70-afd9-7d896f8a2c5e

---
+++ functions/src/ast/FunctionDeclaration.java
@@ -0,0 +1,31 @@
+package ast;
+
+import visitor.Visitor;
+
+public class FunctionDeclaration extends Statement {
+  
+  public final Type returnType;
+  public final String name;
+  public final NodeList<AST> parameters;
+  public final NodeList<Statement> statements;
+  public final Expression returnExpression;
+
+  public FunctionDeclaration(Type returnType,
+                             String name,
+                             NodeList<AST> parameters,
+                             NodeList<Statement> statements,
+                             Expression returnExpression) {
+    
+    super();
+    this.returnType = returnType;
+    this.name = name;
+    this.parameters = parameters;
+    this.statements = statements;
+    this.returnExpression = returnExpression;
+  }
+  
+  @Override
+  public <R> R accept(Visitor<R> v) {
+    return null;
+  }
+}
