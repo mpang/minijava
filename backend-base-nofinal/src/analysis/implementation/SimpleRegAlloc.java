9c219b25e9ffd76290eebb0daf03eb243ed2f759
j6j7 <j6j7@07e144b1-f7ef-4d70-afd9-7d896f8a2c5e>
2014-03-27 06:55:36 +0000

    revert changes
    git-svn-id: https://kunghit.ugrad.cs.ubc.ca:8411/repos/2013w2/users/j6j7@1889 07e144b1-f7ef-4d70-afd9-7d896f8a2c5e

--- backend-base-nofinal/src/analysis/implementation/SimpleRegAlloc.java
+++ backend-base-nofinal/src/analysis/implementation/SimpleRegAlloc.java
@@ -5,7 +5,6 @@ import ir.temp.Color;
 import ir.temp.Temp;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -17,7 +16,6 @@ import util.IndentingWriter;
 import util.List;
 import analysis.FlowGraph;
 import analysis.InterferenceGraph;
-import analysis.InterferenceGraph.Move;
 import analysis.RegAlloc;
 import analysis.util.graph.Node;
 import codegen.AssemProc;
@@ -46,23 +44,8 @@ public class SimpleRegAlloc extends RegAlloc {
 	private final int K;
 	private Set<Temp> precoloured = new HashSet<Temp>();
   private java.util.List<Temp> simplifyCandidates = new ArrayList<Temp>();
-  private java.util.List<Move> coalesceCandidates = new ArrayList<Move>();
-  private java.util.List<Temp> freezeCandidates = new ArrayList<Temp>();
   private java.util.List<Temp> spillCandidates = new ArrayList<Temp>();
   
-  private Set<Move> coalescedMoves = new HashSet<Move>();
-  private Set<Move> constrainedMoves = new HashSet<Move>();
-  private Set<Move> frozenMoves = new HashSet<Move>();
-  private Set<Move> activeMoves = new HashSet<Move>();
-  /**
-   * mapping between temp and its associated moves
-   */
-  private Map<Temp, Set<Move>> tempMoveMapping = new HashMap<Temp, Set<Move>>();
-  /**
-   * key: temps that are coalesced; value: temp that represent the coalesced one
-   */
-  private Map<Temp, Temp> alias = new HashMap<Temp, Temp>();
-
 	@Override
 	public void dump(IndentingWriter out) {
 		out.println(trace);
@@ -172,15 +155,10 @@ public class SimpleRegAlloc extends RegAlloc {
 	private void process() {
 	  prepareForAllocation();
 	  
-	  while (!simplifyCandidates.isEmpty() || !coalesceCandidates.isEmpty() ||
-	         !freezeCandidates.isEmpty() || !spillCandidates.isEmpty()) {
+	  while (!simplifyCandidates.isEmpty() || !spillCandidates.isEmpty()) {
 	    
 	    if (!simplifyCandidates.isEmpty()) {
 	      simplify();
-	    } else if (!coalesceCandidates.isEmpty()) {
-	      coalesce();
-	    } else if (!freezeCandidates.isEmpty()) {
-	      freeze();
 	    } else {
 	      selectSpill();
 	    }
@@ -188,24 +166,12 @@ public class SimpleRegAlloc extends RegAlloc {
 	}
 	
 	private void prepareForAllocation() {
-	  for (Move move : ig.moves()) {
-      coalesceCandidates.add(move);
-      for (Temp temp : Arrays.asList(move.src.wrappee(), move.dst.wrappee())) {
-        if (!tempMoveMapping.containsKey(temp)) {
-          tempMoveMapping.put(temp, new HashSet<Move>());
-        }
-        tempMoveMapping.get(temp).add(move);
-      }
-    }
-	  
 	  for (Node<Temp> node : ig.nodes()) {
 	    Temp temp = node.wrappee();
 	    if (temp.getColor() != null) {
         precoloured.add(temp);
       } else if (node.outDegree() >= K) {
         spillCandidates.add(temp);
-      } else if (tempMoveMapping.containsKey(temp)) {
-        freezeCandidates.add(temp);
       } else {
         simplifyCandidates.add(temp);
       }
@@ -219,52 +185,13 @@ public class SimpleRegAlloc extends RegAlloc {
     checkSpill();
 	}
 	
-	/**
-	 * 
-	 * @param temp
-	 * @return moves related to a temp that are not coalesced, constrained, or frozen
-	 */
-	private Set<Move> nodeMoves(Temp temp) {
-	  if (!tempMoveMapping.containsKey(temp)) {
-	    return new HashSet<Move>();
-	  }
-	  
-	  Set<Move> allMoves = new HashSet<Move>(tempMoveMapping.get(temp));
-	  Set<Move> coalescedCandidatesCopy = new HashSet<Move>(coalesceCandidates);
-	  coalescedCandidatesCopy.addAll(activeMoves);
-	  allMoves.retainAll(coalescedCandidatesCopy);
-	  return allMoves;
-	}
-	
-	private void enableMoves(java.util.List<Temp> temps) {
-	  for (Temp temp : temps) {
-	    for (Move move : nodeMoves(temp)) {
-	      if (activeMoves.contains(move)) {
-	        activeMoves.remove(move);
-	        coalesceCandidates.add(move);
-	      }
-	    }
-	  }
-	}
-	
 	private void checkSpill() {
     Iterator<Temp> iterator = spillCandidates.iterator();
     while (iterator.hasNext()) {
       Temp next = iterator.next();
       
       if (ig.nodeFor(next).outDegree() < K) {
-        java.util.List<Temp> temps = new ArrayList<Temp>(Arrays.asList(next));
-        for (Node<Temp> neighbours : ig.nodeFor(next).succ()) {
-          temps.add(neighbours.wrappee());
-        }
-        enableMoves(temps);
-        
-        if (nodeMoves(next).isEmpty()) {
-          simplifyCandidates.add(next);
-        } else {
-          freezeCandidates.add(next);
-        }
-        
+        simplifyCandidates.add(next);
         iterator.remove();
       }
     }
@@ -273,109 +200,7 @@ public class SimpleRegAlloc extends RegAlloc {
 	private void selectSpill() {
 	  Temp temp = spillCandidates.remove(0);
 	  simplifyCandidates.add(temp);
-	  freezeMoves(temp);
   }
-	
-	private Temp getAlias(Temp temp) {
-	  if (alias.containsKey(temp)) {
-	    return getAlias(alias.get(temp));
-	  }
-	  
-	  return temp;
-	}
-	
-	private void addToCandidates(Temp temp) {
-	  if (!precoloured.contains(temp) && nodeMoves(temp).isEmpty() && ig.nodeFor(temp).outDegree() < K) {
-	    freezeCandidates.remove(temp);
-	    simplifyCandidates.add(temp);
-	  }
-	}
-	
-	private boolean canCoalesceTemps(Temp a, Temp b) {
-	  int count = 0;
-	  Set<Node<Temp>> nodes = new HashSet<Node<Temp>>();
-	  for (Node<Temp> node : ig.nodeFor(a).succ().append(ig.nodeFor(b).succ())) {
-	    nodes.add(node);
-	  }
-	  
-	  for (Node<Temp> node : nodes) {
-	    if (node.outDegree() >= K) {
-	      count++;
-	    }
-	  }
-	  
-	  return count < K;
-	}
-	
-	private void coalesce(Temp a, Temp b) {
-	  if (freezeCandidates.contains(b)) {
-	    freezeCandidates.remove(b);
-	  } else {
-	    spillCandidates.remove(b);
-	  }
-	  
-	  alias.put(b, a);
-	  tempMoveMapping.get(a).addAll(tempMoveMapping.get(b));
-	  enableMoves(Arrays.asList(b));
-	  ig.merge(ig.nodeFor(a), ig.nodeFor(b));
-	  checkSpill();
-	  if (ig.nodeFor(a).outDegree() >= K && freezeCandidates.contains(a)) {
-	    freezeCandidates.remove(a);
-	    spillCandidates.add(a);
-	  }
-	}
-	
-	private void coalesce() {
-	  Move move = coalesceCandidates.remove(0);
-	  Temp src = getAlias(move.src.wrappee());
-	  Temp dst = getAlias(move.dst.wrappee());
-	  
-	  if (src.equals(dst)) {
-      coalescedMoves.add(move);
-      addToCandidates(src);
-    } else if ((precoloured.contains(src) && precoloured.contains(dst)) || ig.nodeFor(src).goesTo(ig.nodeFor(dst))) {
-      constrainedMoves.add(move);
-      addToCandidates(src);
-      addToCandidates(dst);
-    } else if (canCoalesceTemps(src, dst)) {
-      coalescedMoves.add(move);
-      
-      if (precoloured.contains(dst)) {
-        coalesce(dst, src);
-        addToCandidates(dst);
-      } else {
-        coalesce(src, dst);
-        addToCandidates(src);
-      }
-      
-      //coalesce(src, dst);
-      //addToCandidates(src);
-    } else {
-      activeMoves.add(move);
-    }
-	}
-	
-	private void freeze() {
-	  Temp temp = freezeCandidates.remove(0);
-	  simplifyCandidates.add(temp);
-	  freezeMoves(temp);
-	}
-	
-	private void freezeMoves(Temp temp) {
-	  for (Move move : nodeMoves(temp)) {
-      Temp src = getAlias(move.src.wrappee());
-      Temp dst = getAlias(move.dst.wrappee());
-      Temp anotherTemp = getAlias(temp).equals(src) ? dst : src;
-      
-      activeMoves.remove(move);
-      frozenMoves.add(move);
-      
-      if (freezeCandidates.contains(anotherTemp) && nodeMoves(anotherTemp).isEmpty()) {
-        freezeCandidates.remove(anotherTemp);
-        simplifyCandidates.add(anotherTemp);
-      }
-    }
-	}
 	
 	private Color getColor(Node<Temp> node) {
 		return getColor(node.wrappee());
