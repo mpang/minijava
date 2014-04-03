586b0de77af040259e66c5d2e470dffdbc3a10ee
j6j7 <j6j7@07e144b1-f7ef-4d70-afd9-7d896f8a2c5e>
2014-04-03 09:16:05 +0000

    sample files for instanceof
    git-svn-id: https://kunghit.ugrad.cs.ubc.ca:8411/repos/2013w2/users/j6j7@2033 07e144b1-f7ef-4d70-afd9-7d896f8a2c5e

---
+++ minijava/sample/instanceof.java
@@ -0,0 +1,65 @@
+class InstanceOf {
+  public static void main(String[] a) {
+    System.out.println(new Main().Start());
+  }
+}
+
+class Car {
+  int code;
+
+  public int Init() {
+    code = 1;
+    return 0;
+  }
+
+  public int PrintCode() {
+    System.out.println(code);
+    return 0;
+  }
+}
+
+class Suv extends Car {
+  public int Init() {
+    code = 2;
+    return 0;
+  }
+}
+
+class Limo extends Car {
+  public int dummy() {
+    return 0;
+  }
+}
+
+class Main {
+  public int Start() {
+    Car car;
+    Car suv;
+    Car limo;
+    int temp;
+
+    car = new Car();
+    suv = new Suv();
+    limo = new Limo();
+
+    temp = car.Init();
+    temp = suv.Init();
+
+    temp = car.PrintCode();
+    temp = suv.PrintCode();
+
+    if (suv instanceof Suv) {
+      System.out.println(3);
+    } else {
+      System.out.println(4);
+    }
+    
+    if (limo instanceof Suv) {
+      System.out.println(5);
+    } else {
+      System.out.println(6);
+    }
+
+    return 0;
+  }
+}
